a44904dae826fed650e282b2055e59302b6966a1
SmorenoContreras <SMorenoContreras@gmail.com>
2023-12-05 20:45:40 -0800

    intial commit

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Workday Scheduler
+
+## Description
+
+Scroll through the various times and set whatever you need to do by typing text into space and clicking the button. 
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+No need to install anything except a browser that can read html/javascript
+
+## Usage
+
+for planning and scheduling work day
+
+    ```md
+    ![workday screenshot](./screenshot/workdayscreenshot.png)
+    ```
+
+## Credits
+
+Ask BcS, W3 schools, MDN, Ricardo Torres-Castro
+
+## License
+
+None
---
+++ script.js
@@ -0,0 +1,48 @@
+//Displays current time.
+var worktime = dayjs().hour()
+
+//function for button also sets items for storage
+$(function () {
+
+  $(".saveBtn").on('click', function () {
+
+    var key = $(this).parent().attr('id')
+
+    var value = $(this).siblings('textarea').val()
+
+    localStorage.setItem(key, value)
+
+  })
+
+//this is to run through the time block and add classes depending on time
+$('.time-block').each(function () {
+
+  var timeBlockHour = parseInt($(this).attr('id').split('-')[1])
+
+  if (timeBlockHour < worktime) {
+
+    $(this).addClass("past")
+  } else if (timeBlockHour === worktime) {
+    
+    $(this).addClass("present")
+  } else {
+    $(this).addClass("future")
+  }
+
+})
+
+// gets from local storage
+$("#hour-9 textarea").val(localStorage.getItem("hour-9"))
+$("#hour-10 textarea").val(localStorage.getItem("hour-10"))
+$("#hour-11 textarea").val(localStorage.getItem("hour-11"))
+$("#hour-12 textarea").val(localStorage.getItem("hour-12"))
+$("#hour-13 textarea").val(localStorage.getItem("hour-13"))
+$("#hour-14 textarea").val(localStorage.getItem("hour-14"))
+$("#hour-15 textarea").val(localStorage.getItem("hour-15"))
+$("#hour-16 textarea").val(localStorage.getItem("hour-16"))
+$("#hour-17 textarea").val(localStorage.getItem("hour-17"))
+
+//Jquery that displays the current time with month day and year
+$('#currentDay').text(dayjs().format('MMM DD YYYY'))
+
+});
